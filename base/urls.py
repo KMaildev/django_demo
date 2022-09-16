7f6b6caa7c81fd44c80af94640b92bc8315ef0e3
ZinMinTun <kmailsoftwaredevelopment@gmail.com>
2022-09-16 23:52:37 +0630

    first commit

---
+++ base/urls.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login', views.loginPage, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name="create_room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update_room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete_room"),
+]
